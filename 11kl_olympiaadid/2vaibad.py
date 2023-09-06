4f6fff16a18ac2388d823d25b236c38ebc80ec8d
Gustav <64610274+sego-w@users.noreply.github.com>
2023-09-06 11:02:06 +0300

    ei toota tho
    RAW CODE OFFICIAL

---
+++ 11kl_olympiaadid/2vaibad.py
@@ -0,0 +1,23 @@
+def main():
+    vaipmap = open("/Users/gustavtamkivi/Documents/Code/someschoolting/11kl_olympiaadid/vaibad.txt","r+").read()
+    lists = vaipmap.split("\n")
+    v = []
+    q = []
+    x = 0 
+    for i in range(int(lists[0][0])+1):
+        q.append((lists[i].split()))
+    print(q)
+    for e in range(len(q)-1):
+        width = int(q[0][1]) - int(q[e][2]) - int(q[e][3])
+        height = int(q[0][2]) - int(q[e][0]) - int(q[e][1])
+        area = width * height
+        v.append(area)
+    for number in v:
+        x += int(number)
+    print(f"Vaipade kogu pindala on {x} yhikut!")
+    
+    
+    
+
+main()
+

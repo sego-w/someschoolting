9306ba5ccaa7e5f326ee8dda41e7d4c343c79082
Gustav <64610274+sego-w@users.noreply.github.com>
2023-09-13 10:55:39 +0300

    patched
    fix

--- 11kl_olympiaadid/2vaibadlahendus.py
+++ 11kl_olympiaadid/2vaibadlahendus.py
@@ -4,7 +4,7 @@ f=open("/Users/gustavtamkivi/Documents/Code/someschoolting/11kl_olympiaadid/vaib
 K,L,P = [int(jupp) for jupp in f.readline().strip().split(" ")]
 
 m=[[0]*L for nr in range(P)]
-print(m)
+
 
 for nr in range(K):
     N,S,E,W = [int(jupp) for jupp in f.readline().strip().split(" ")]
@@ -15,10 +15,10 @@ for nr in range(K):
             m[rida][veerg] = 1
         
 # print("\n".join([str(lahter) for lahter in rida]) for rida in m)
-
+'''
 for rida in m:
     print(*[str(lahter) for lahter in rida])
-
+'''
 x=0
 for rida in m:
     for lahter in rida:
@@ -28,3 +28,8 @@ print(f"Vaibad katavad kogupindaala {x} ruutyhikut")
 
 
 
+'''
+2 8 9
+3 3 3 0
+5 1 2 4
+'''

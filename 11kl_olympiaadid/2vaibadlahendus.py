87a4084ca1882c17da862d7f74053cad09ff5c85
Gustav <64610274+sego-w@users.noreply.github.com>
2023-09-13 10:31:25 +0300

    added working solution
    we done did dis shii in class doe

---
+++ 11kl_olympiaadid/2vaibadlahendus.py
@@ -0,0 +1,30 @@
+# opetajaga koos tehtud
+
+f=open("/Users/gustavtamkivi/Documents/Code/someschoolting/11kl_olympiaadid/vaibad.txt")
+K,L,P = [int(jupp) for jupp in f.readline().strip().split(" ")]
+
+m=[[0]*L for nr in range(P)]
+print(m)
+
+for nr in range(K):
+    N,S,E,W = [int(jupp) for jupp in f.readline().strip().split(" ")]
+    
+#   print((L-E-W)*(P-N-S))
+    for veerg in range(W, L-E):
+        for rida in range(N, P-S):
+            m[rida][veerg] = 1
+        
+# print("\n".join([str(lahter) for lahter in rida]) for rida in m)
+
+for rida in m:
+    print(*[str(lahter) for lahter in rida])
+
+x=0
+for rida in m:
+    for lahter in rida:
+        x+=lahter
+
+print(f"Vaibad katavad kogupindaala {x} ruutyhikut")
+
+
+

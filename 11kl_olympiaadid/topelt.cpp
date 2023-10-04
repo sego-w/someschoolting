217924117fd4ae9879912413f14d9ed63cee7815
Gustav <64610274+sego-w@users.noreply.github.com>
2023-10-04 10:30:01 +0300

    lol

---
+++ 11kl_olympiaadid/topelt.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int number;
+
+    
+    cin >> number;
+
+    number *= 2;
+
+    cout << number;
+
+    return 0;
+
+}
+
---
+++ 11kl_olympiaadid/topelt.py
@@ -0,0 +1,5 @@
+def main():
+    z = int(input())
+    return z*2
+
+print(main())
